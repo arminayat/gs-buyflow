97f7386fffe01d2e160dd4e34599e9598ee10aed
Armin Ayatollahi <arminayat3@gmail.com>
2022-07-01 21:33:47 +0200

    refactor(buyflow): update callback function > in order to support updating multiple fields at once

--- src/buyflow/Buyflow.tsx
+++ src/buyflow/Buyflow.tsx
@@ -18,20 +18,20 @@ const Buyflow: React.FC<BuyflowProps> = ({ productId }) => {
     email: '',
     age: 0,
   })
-  const getStepCallback = (nextStep: string) => (field: string, value: any) => {
-    updateData({ ...collectedData, [field]: value })
+  const getStepCallback = (nextStep: string) => (newData: {
+    [key: string]: string | number
+  }) => {
+    updateData({ ...collectedData, ...newData })
     setStep(nextStep)
   }
   return (
     <>
       <h4>Buying {PRODUCT_IDS_TO_NAMES[productId]}</h4>
-      {(currentStep === 'email' && <EmailStep cb={getStepCallback('age')} />) ||
-        (currentStep === 'age' && (
-          <AgeStep cb={getStepCallback('summary')} />
-        )) ||
-        (currentStep === 'summary' && (
-          <SummaryStep collectedData={collectedData} />
-        ))}
+      {currentStep === 'email' && <EmailStep cb={getStepCallback('age')} />}
+      {currentStep === 'age' && <AgeStep cb={getStepCallback('summary')} />}
+      {currentStep === 'summary' && (
+        <SummaryStep collectedData={collectedData} />
+      )}
     </>
   )
 }
--- src/buyflow/steps/AgeStep.tsx
+++ src/buyflow/steps/AgeStep.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 
 interface AgeStepProps {
-  cb: (field: string, value: number) => void
+  cb: (newData: { [key: string]: string | number }) => void
 }
 
 const AgeStep: React.FC<AgeStepProps> = (props) => {
@@ -18,7 +18,7 @@ const AgeStep: React.FC<AgeStepProps> = (props) => {
           value={age}
         ></input>
       </div>
-      <button onClick={() => props.cb('age', age)}>Next</button>
+      <button onClick={() => props.cb({ age: age })}>Next</button>
     </>
   )
 }
