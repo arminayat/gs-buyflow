a44da1795794fa9e97c399783319845c883ae44d
Armin Ayatollahi <arminayat3@gmail.com>
2022-07-03 23:28:44 +0200

    feat(constants): add product ids and product anmes mapping

---
+++ src/constants/product.ts
@@ -0,0 +1,6 @@
+import { ProductIds } from '../buyflow/types'
+
+export const PRODUCT_IDS_TO_NAMES = {
+  [ProductIds.developerInsurance]: 'Developer Insurance',
+  [ProductIds.designerInsurance]: 'Designer Insurance',
+}
