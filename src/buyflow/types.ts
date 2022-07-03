f96159bc18c490f78d0b9d995e67465ee395133a
Armin Ayatollahi <arminayat3@gmail.com>
2022-07-03 23:27:39 +0200

    feat(buyflow/types): add steps enum

--- src/buyflow/types.ts
+++ src/buyflow/types.ts
@@ -2,3 +2,10 @@ export enum ProductIds {
   developerInsurance = 'dev_ins',
   designerInsurance = 'des_ins',
 }
+
+export enum BuyflowSteps {
+  name = 'NAME',
+  email = 'EMAIL',
+  age = 'AGE',
+  summary = 'SUMMARY',
+}
