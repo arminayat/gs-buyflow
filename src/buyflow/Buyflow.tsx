0e5dd7d3d9035aa0aa6da2f5eecd3d5a9fd477c2
Armin Ayatollahi <arminayat3@gmail.com>
2022-07-04 10:02:46 +0200

    refactor(Buyflow): extract steps handling logic to Steps component

--- src/buyflow/Buyflow.tsx
+++ src/buyflow/Buyflow.tsx
@@ -1,52 +1,39 @@
 import React, { useState } from 'react'
 import { PRODUCT_IDS_TO_NAMES } from '../constants/product'
-import useGetProductBuyflowSteps from './hooks/useGetProductBuyflowSteps'
-import AgeStep from './steps/AgeStep'
-import EmailStep from './steps/EmailStep'
-import NameStep from './steps/NameStep'
-import SummaryStep from './steps/SummaryStep'
-import { BuyflowSteps, ProductIds } from './types'
+import Steps from './steps'
+import { ProductIds } from './types'
 
 interface BuyflowProps {
   productId: ProductIds
 }
 
-const Buyflow: React.FC<BuyflowProps> = ({ productId }) => {
-  const buyflowSteps = useGetProductBuyflowSteps(productId)
+export interface ICollectedData {
+  firstname?: string
+  lastname?: string
+  email: string
+  age: number
+}
 
-  const [collectedData, setCollectedData] = useState({
+const Buyflow: React.FC<BuyflowProps> = ({ productId }) => {
+  const [collectedData, setCollectedData] = useState<ICollectedData>({
     firstname: '',
     lastname: '',
     email: '',
     age: 0,
   })
 
-  const [currentStep, setStep] = useState(buyflowSteps[0])
-  const handleNextStep = () => {
-    let nextStepIndex = buyflowSteps.indexOf(currentStep) + 1
-    setStep(buyflowSteps[nextStepIndex])
-  }
-
-  const handleSubmit = (newData: { [key: string]: string | number }) => {
+  const handleDataSubmit = (newData: { [key: string]: string | number }) => {
     setCollectedData({ ...collectedData, ...newData })
-    handleNextStep()
   }
 
   return (
     <>
       <h4>Buying {PRODUCT_IDS_TO_NAMES[productId]}</h4>
-      {currentStep === BuyflowSteps.name && (
-        <NameStep handleSubmit={handleSubmit} />
-      )}
-      {currentStep === BuyflowSteps.email && (
-        <EmailStep handleSubmit={handleSubmit} />
-      )}
-      {currentStep === BuyflowSteps.age && (
-        <AgeStep handleSubmit={handleSubmit} />
-      )}
-      {currentStep === BuyflowSteps.summary && (
-        <SummaryStep collectedData={collectedData} productId={productId} />
-      )}
+      <Steps
+        productId={productId}
+        handleDataSubmit={handleDataSubmit}
+        collectedData={collectedData}
+      />
     </>
   )
 }
